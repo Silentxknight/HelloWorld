817d5a13ab0e013ea83e65d8516e416f5d67ec6d
Silent_Coder <36187018+Silentxknight@users.noreply.github.com>
2019-05-12 17:35:15 +0530

    Add files via upload

---
+++ New_ab.java
@@ -0,0 +1,55 @@
+package par;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class New_ab {
+
+public static void main(String[]args) {
+int rg; String op;
+rg=8;
+ab.gone();
+op=String.valueOf(rg);
+System.out.println("hh"+op);
+
+
+
+Scanner sc=new Scanner(System.in);
+
+//System.out.println("enter the number");
+//int a=sc.nextInt(); 
+	for(int a=0;a<=127;a++) {
+	
+System.out.printf("%d :    %c  \n",a,a);
+
+
+}}
+
+}
+	
+
+class ab{
+ static	void Name() {
+		int i;
+		for (;;) {
+			
+			System.out.println("lol");
+		}
+	}
+	static void gone() {
+		int i=2;
+		while(i<99) {
+			try {
+				TimeUnit.MILLISECONDS.sleep(1);
+			} catch (Exception e) {
+			
+				e.printStackTrace();
+			}
+			System.out.println("infinty");
+		}
+	}
+}
+
+
+
+	
+
